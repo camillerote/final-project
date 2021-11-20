fa3abffd375aa7d7454b49007eebe1f83875ab81
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-11-20 03:07:51 +0000

    scheduler.c

--- main.c
+++ main.c
@@ -1,73 +1,112 @@
-#include <stdio.h>
+include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #define SIZE 256
-
-
 
 /* function to find day*/ 
 
 typedef struct agenda {
-	int day;
-	char event1[SIZE]; //hard coded event size
-	char event2[SIZE]; //hard coded event size
-	char event3[SIZE]; //hard coded event size
+	char events[10][SIZE];
+	int numOfEvents;
 } Agenda;
-int main(){
+
+
+int getIntegerOfDay(char *day){
+	if( strcmp(day, "sunday") == 0 ) return 0;
+	if( strcmp(day, "monday") == 0 ) return 1;
+	if( strcmp(day, "tuesday") == 0 ) return 2;
+	if( strcmp(day, "wednesday") == 0 ) return 3;
+	if( strcmp(day, "thursday") == 0 ) return 4;
+	if( strcmp(day, "friday") == 0 ) return 5;
+	if( strcmp(day, "saturday") == 0 ) return 6;
+	else return -1;
+}
+
+void printDay(int dayInt){
+	if( dayInt==0 ) printf("Sunday \n --------\n");
+	if( dayInt==1 ) printf("Monday \n --------\n");
+	if( dayInt==2 ) printf("Tuesday \n --------\n");
+	if( dayInt==3 ) printf("Wednesday \n --------\n");
+	if( dayInt==4 ) printf("Thursday \n --------\n");
+	if( dayInt==5 ) printf("Friday \n --------\n");
+	if( dayInt==6 ) printf("Saturday \n --------\n");
+
+}int main(){
+	struct agenda Agendas[7]; // each index here is day of week (0 is sunday)/
+	int alive = 1;
+
+	// Initialize the numOfEvents to make sure its set to 0/
+	// so ervetythign else works/
+	for(int i = 0; i<7; i++){
+		Agendas[i].numOfEvents = 0;
+	}
+	while(alive){
+		printf("\e[0;33m \n> \e[0;37m");
+		char userInput[200];
+		char *token;
+		const char s[2] = " ";
+
+		gets(userInput);
+		token = strtok(userInput, s);
+		/* get the first token */
+
+		// strcmp returns 0 if the two inputs are a match/
+		if( strcmp(token, "help") == 0 ){
+ 			printf("add [day] [event] , Add event to day\n");
+			printf("show [day/'all'] , show all the events for selected day, or for 'all'");
+			printf("edit [day] [eventNumber] [task], edit a day's event indexed by eventNumber "); 	
+	 }
+	if( strcmp(token, "add") == 0 ){
+		char *day = strtok(NULL, s); // grab second arg
+		char *task = strtok(NULL, s); // grab 3rd arg (but its a single word, woops)
+		int dayInt = getIntegerOfDay(day); // encodes our day to a number, use for indexing
+		
+		if(dayInt == -1){
+			printf("\n Entered invalid day");
+			continue;
+	}
+
+	// This day has numOfEvents events
+	int numOfEvents = Agendas[dayInt].numOfEvents;
 	
-{
-///have to make a recursion
-	printf("Please enter a number for a day\n");
-	printf("1) Sunday\n");
-	printf("2) Monday\n");
-	printf("3) Tuesday\n");
-  printf("4) Wednesday\n"); 
- 
-	int day;
-	scanf("%d", &day);
-	switch(day){
-		case 1:
-			printf("you entered Sunday\n");
-			break;
-		case 2:
-			printf("you entered Monday\n");
-			break;
-    case 3:
-      printf("you entered Tuesday");
-		default:
-			break;
+	if (numOfEvents > 9){ 
+		printf("Too many events on day %s", day);
+		continue;
 	}
-for(int day=0; day<7; day++)
-{
-  Agenda agendas[7]; //one for each day of the week
-				// agendas[0] = Sunday
-				// agendas[1] = Monday
-    
-      char buffer[SIZE];
-        char* bufptr = buffer;
-        size_t bufsize = SIZE;
-        size_t characters;
+	// We want to write our new task to the next open spot, which is index numOfEvents
+	strcpy(Agendas[dayInt].events[numOfEvents], task);
 
-
-	for(int i=0; i< 7; i++){
-	       	printf("Enter something", agendas[i]);
-	       	fflush(stdin);
-	       	getline(&bufptr,&bufsize,stdin);
-	       	strcpy(agendas[i].event1, buffer);
-	       	printf("youe entered %s\n", agendas[i].event1);
-
-	       	printf("have a nice day\n", agendas );
+	// Make sure we increase the numOfEvents of this specific day
+	// so we dont overwrite it when we add another task to this day
+	Agendas[dayInt].numOfEvents++;
+	printf("New task: %s\n", Agendas[dayInt].events[numOfEvents]);
 	}
 
-printf("printing all agendas for the week\n");
-	for(int i=0; i< 7; i++){
-		printf("%s\n", agendas[i].event1);
-}
+	if( strcmp(token, "show") == 0){
+		char *day = strtok(NULL, s);
+		int dayInt = getIntegerOfDay(day); // encodes our day to a number, use for indexing
 
-	 
+		if( strcmp(day, "all") == 0){
 
-	//print_day(sunday);
-	return 0;
-}
-}
-}
+			// Print out every single day and all their events
+			for(int i=0; i<7; i++){ // Day loop
+				printDay(i);
+				for(int j=0; j < Agendas[i].numOfEvents; j++){ // Event loop
+					printf("%d : %s\n", j+1, Agendas[i].events[j] );
+				}
+				printf("\n");
+			}
+			continue;
+		}	
+
+		if(dayInt == -1){
+			printf("\n Entered invalid day");
+			continue;
+		}
+
+		// print the events for this day
+		for( int i = 0; i < Agendas[dayInt].numOfEvents; i++){
+			printf("%s\n", Agendas[dayInt].events[i] );
+		}	
+	}
